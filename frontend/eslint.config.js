82eaa22b4f613e11daa5c9fbff8eaf84eb1b8268
NikolaT380 <nikolatanevski23@gmail.com>
2026-06-27 15:04:39 +0200

    change eslint rules to warn

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -19,5 +19,12 @@ export default defineConfig([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      'react-hooks/exhaustive-deps': 'warn',
+      'react-hooks/rules-of-hooks': 'warn',
+      'react-refresh/only-export-components': 'warn',
+    },
   },
 ])
